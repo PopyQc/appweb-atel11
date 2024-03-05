75bbb94ed8b8f402618529b0612620efecac338f
Raphaël Boudreault <raphael.boudreault@gmail.com>
2024-03-05 13:25:10 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import Timeline from './components/Timeline.vue'
+  import "bootstrap/dist/css/bootstrap.min.css"
+  import "bootstrap"
+</script>
+
+<template>
+  <div class="container">
+    <Timeline />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
